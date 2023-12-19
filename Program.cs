753b734031ac616eba76970f89e3bb3702f74ba1
sufferintoilet <nguyenhuynhquocan1@gmail.com>
2023-12-19 22:12:56 +0900

    sum

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sumList = 0;
+        int[] myList = {1,2,3,4,5,6,7,8,9,10};
+        Console.WriteLine("summmary");
+        for (int i = 0; i <= myList.Length-1; i++) 
+        {
+            sumList += myList[i]; 
+        }
+        Console.WriteLine(sumList);
+}       
+
+
+}
